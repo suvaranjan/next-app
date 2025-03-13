ebde491d9312fe65a7eb751f91b50a3e4c85b25e
suvaranjan <suvaranjan01@gmail.com>
2025-03-13 08:38:25 +0530

    Added pdf-viewer component v3

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,9 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
-// next.config.js
 import { withContentlayer } from "next-contentlayer2";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true };
+const nextConfig = {
+  reactStrictMode: true,
+  // Remove swcMinify (no longer needed)
+};
 
-module.exports = withContentlayer(nextConfig);
-
-// import { withContentlayer } from "next-contentlayer2";
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: false, // Keep this as `true` unless you encounter issues
-//   experimental: {
-//     turbo: {
-//       resolveAlias: {
-//         canvas: "./empty-module.ts", // Resolve canvas to an empty module
-//       },
-//     },
-//   },
-// };
-
-// module.exports = withContentlayer(nextConfig);
+export default withContentlayer(nextConfig);
