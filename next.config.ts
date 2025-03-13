1c49e176a9c9626829a1502b7ee05038843aa2c9
suvaranjan <suvaranjan01@gmail.com>
2025-03-13 08:55:03 +0530

    Added pdf-viewer component v7

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,24 @@
+// import { withContentlayer } from "next-contentlayer2";
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   // Remove swcMinify (no longer needed)
+// };
+
+// export default withContentlayer(nextConfig);
+
 import { withContentlayer } from "next-contentlayer2";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Remove swcMinify (no longer needed)
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...config.externals, "canvas"];
+    }
+    return config;
+  },
 };
 
 export default withContentlayer(nextConfig);
